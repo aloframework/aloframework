36c886c1fde282df962ef3fa40633ef96a51dc27
Alorel <a.molcanovas@gmail.com>
2015-09-15 23:15:53 +0100

    aloframework/core @ required

---
+++ README.md
@@ -0,0 +1,9 @@
+# AloFramework #
+
+This is a framework in building.
+
+[![License](https://poser.pugx.org/aloframework/aloframework/license?format=plastic)](LICENSE) [![Latest Stable Version](https://poser.pugx.org/aloframework/aloframework/v/stable?format=plastic)](https://packagist.org/packages/aloframework/aloframework)  [![Total Downloads](https://poser.pugx.org/aloframework/aloframework/downloads?format=plastic)](https://packagist.org/packages/aloframework/aloframework)
+
+Development code quality: [![SensioLabsInsight](https://insight.sensiolabs.com/projects/2b373436-ce4e-4d46-a217-a99645ffa5a0/small.png)](https://insight.sensiolabs.com/projects/2b373436-ce4e-4d46-a217-a99645ffa5a0)
+
+Dev: [![Dev Build Status](https://travis-ci.org/aloframework/aloframework.svg?branch=develop)](https://travis-ci.org/aloframework/aloframework) Master: [![Dev Build Status](https://travis-ci.org/aloframework/aloframework.svg?branch=master)](https://travis-ci.org/aloframework/aloframework) 
---
+++ src/index.php
@@ -0,0 +1,32 @@
+<?php
+
+    /** Microtime float when the script started */
+    define('GEN_START', microtime(true));
+
+    /** root/index.php directory */
+    define('DIR_INDEX', __DIR__ . DIRECTORY_SEPARATOR);
+
+    require_once 'sys' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'global_defines.php';
+
+    // ===== General setup BEGIN =====
+
+    /** The application environment. Valid values are ENV_SETUP, ENV_DEVELOPMENT and ENV_PRODUCTION */
+    define('ENVIRONMENT', ENV_SETUP);
+
+    /** Defines the log level. Valid values are LOG_LEVEL_NONE LOG_LEVEL_DEBUG, LOG_LEVEL_WARNING and LOG_LEVEL_ERROR */
+    define('LOG_LEVEL', LOG_LEVEL_WARNING);
+
+    /**
+     * If logging is set to intense, an entire debug backtrace will be appended to each row, otherwise it's just the
+     * last file and line number
+     */
+    define('LOG_INTENSE', false);
+
+    // ===== General setup END. You needn't change anything below this line =====
+
+    // Change dir to make sure CLI requests are correct
+    if (defined('STDIN')) {
+        chdir(dirname(__FILE__));
+    }
+
+    require_once DIR_SYS . 'core' . DIRECTORY_SEPARATOR . 'core.php';
